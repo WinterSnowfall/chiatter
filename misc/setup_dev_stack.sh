41ca61f6342dddc4f303e205d22dfa5113552814
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2022-10-23 16:18:23 +0300

    Use the latest stable Alpine-based grafana image

--- misc/setup_dev_stack.sh
+++ misc/setup_dev_stack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker pull prom/prometheus
-docker pull grafana/grafana:main-ubuntu
+docker pull grafana/grafana
 
 docker run -d --name prometheus --net=host -v prometheus:/prometheus -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --storage.tsdb.retention.time=1y --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.console.libraries=/usr/share/prometheus/console_libraries --web.console.templates=/usr/share/prometheus/consoles
-docker run -d --name grafana --net=host -v $PWD/datasources.yml:/etc/grafana/provisioning/datasources/datasources.yml grafana/grafana:main-ubuntu
+docker run -d --name grafana --net=host -v $PWD/datasources.yml:/etc/grafana/provisioning/datasources/datasources.yml grafana/grafana
 
